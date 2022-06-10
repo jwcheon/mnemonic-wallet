955167c8854af18d9a38802e9d82dd48d46170e1
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-06-10 11:33:03 +0900

    Translate KR > ENG

--- routes/wallet.js
+++ routes/wallet.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const lightwallet = require("eth-lightwallet");
 const fs = require('fs');
 
-// TODO : lightwallet 모듈을 사용하여 랜덤한 니모닉 코드를 얻습니다.
+// TODO : use lightwallet module to create random mnemonic code
 router.post('/newMnemonic', async(req,res) => {
 
 });
 
 
-// TODO : 니모닉 코드와 패스워드를 이용해 keystore와 address를 생성합니다.
+// TODO : use mnemonic code and password to create keystore & address
 router.post('/newWallet', async(req, res) => {
 
 });
